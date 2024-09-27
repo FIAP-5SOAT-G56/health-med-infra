ed35c3699b3c651ad91ee8316deb71147df363e2
Matheus Borges <matob@live.com>
2024-09-27 14:21:30 -0300

    fix: fix ECS Service Network connection

--- terraform/ecs_service.tf
+++ terraform/ecs_service.tf
@@ -1,3 +1,32 @@
+resource "aws_security_group" "ecs_service" {
+  name        = "${var.resource_prefix}-health-med-api-sg-ecs-service"
+  description = "Inbound: All + Outbound: All"
+  vpc_id      = aws_vpc.default.id
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = [aws_security_group.health_med_api_lb.id]
+  }
+
+  egress {
+    from_port   = 0             # Allowing any incoming port
+    to_port     = 0             # Allowing any outgoing port
+    protocol    = "-1"          # Allowing any outgoing protocol 
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
+
+  tags = {
+    Name = "${var.resource_prefix}-health-med-api-sg-ecs-service"
+  }
+
+  depends_on = [
+    aws_vpc.default
+  ]
+}
+
 resource "aws_ecs_service" "health_med_api" {
   name                              = "${var.resource_prefix}-health-med-api-ecs-service"
   cluster                           = aws_ecs_cluster.default.id
@@ -15,13 +44,12 @@ resource "aws_ecs_service" "health_med_api" {
       aws_subnet.subnet_public_a.id,
       aws_subnet.subnet_public_b.id
     ]
-
     security_groups = [
       aws_security_group.health_med_api_rds.id,
-      aws_security_group.health_med_api_cache.id
+      aws_security_group.health_med_api_cache.id,
+      aws_security_group.ecs_service.id
     ]
-
-    assign_public_ip = false
+    assign_public_ip = true
   }
 
   load_balancer {
